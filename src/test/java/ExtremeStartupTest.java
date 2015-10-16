3fd2ff8d47fe0ca4bcbc5d42f9051e23c81fd6ef
Devin Witherspoon <dwitherspoon@palantir.com>
2015-10-16 17:47:26 +0100

    Moved to hamcrest

--- src/test/java/ExtremeStartupTest.java
+++ src/test/java/ExtremeStartupTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class ExtremeStartupTest {
 
@@ -8,32 +10,32 @@ public class ExtremeStartupTest {
 
     @Test
     public void should_accept_missing_input() {
-        assertEquals(server.answer(null), "A");
+        assertThat(server.answer(null), is("A"));
     }
 
     @Test
     public void should_add_numbers() {
-        assertEquals(server.answer("what is the sum of 14 and 4"), "18");
+        assertThat(server.answer("what is the sum of 14 and 4"), is("18"));
     }
 
     @Test
     public void should_get_largest_of_two() {
-        assertEquals(server.answer("which of the following numbers is the largest: 12, 15"), "15");
+        assertThat(server.answer("which of the following numbers is the largest: 12, 15"), is("15"));
     }
 
     @Test
     public void should_get_largest_of_three() {
-        assertEquals(server.answer("which of the following numbers is the largest: 12, 15, 17"), "17");
+        assertThat(server.answer("which of the following numbers is the largest: 12, 15, 17"), is("17"));
     }
 
     @Test
     public void should_return_no_cube() {
-        assertEquals(server.answer("which of the following numbers is both a square and a cube: 36, 144"), "None");
+        assertThat(server.answer("which of the following numbers is both a square and a cube: 36, 144"), is("None"));
     }
 
     @Test
     public void shoud_be_sean_connery() {
-        assertEquals(server.answer("who played James Bond in the film Dr No"), "Sean Connery");
+        assertThat(server.answer("who played James Bond in the film Dr No"), is("Sean Connery"));
     }
 
 }
